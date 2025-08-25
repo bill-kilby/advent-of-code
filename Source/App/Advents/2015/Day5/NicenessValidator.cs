ae98ce6f8a71bdfceadfa0560504da8f053a0605
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-26 00:20:39 +0100

    Implement "Niceness" Validator (see question spec)

--- Source/App/Advents/2015/Day5/NicenessValidator.cs
+++ Source/App/Advents/2015/Day5/NicenessValidator.cs
@@ -11,9 +11,56 @@ namespace App.Advents._2015.Day5
     {
         private string _input;
 
+        private HashSet<char> _vowels = new()
+        {
+            'a','e','i','o','u'
+        };
+
+        private HashSet<string> _naughtyStrings = new()
+        {
+            "ab", "cd", "pq", "xy"
+        };
+
         public bool Validate(string value)
         {
-            throw new NotImplementedException();
+            _input = value;
+
+            return ContainsThreeVowels()
+                && ContainsDoubleLetter()
+                && !ContainsNaughtyStrings();
+        }
+
+        private bool ContainsThreeVowels()
+        {
+            var count = 0;
+            foreach (var character in _input)
+            {
+                if (_vowels.Contains(character)) count++;
+            }
+
+            return count >= 3;
+        }
+
+        private bool ContainsDoubleLetter()
+        {
+            var previousLetter = _input[0];
+            for (var i = 1; i < _input.Length; i++)
+            {
+                var previous = _input[i - 1];
+                if (previous == _input[i]) return true;
+            }
+
+            return false;
+        }
+
+        private bool ContainsNaughtyStrings()
+        {
+            foreach (var entry in _naughtyStrings)
+            {
+                if (_input.Contains(entry)) return true;
+            }
+
+            return false;
         }
     }
 }
