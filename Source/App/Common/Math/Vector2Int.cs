005943c746df1e08e2b99a7e4bc19d924ef21729
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-25 22:40:34 +0100

    Add Vector2Int

---
+++ Source/App/Common/Math/Vector2Int.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Common.Math
+{
+    internal class Vector2Int
+    {
+        public int X;
+        public int Y;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Vector2Int other)
+            {
+                return X == other.X && Y == other.Y;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 31 + X;
+                hash = hash * 31 + Y;
+                return hash;
+            }
+        }
+
+        public static bool operator ==(Vector2Int left, Vector2Int right)
+        {
+            if (ReferenceEquals(left, right))
+                return true;
+            if (left is null || right is null)
+                return false;
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Vector2Int left, Vector2Int right)
+        {
+            return !(left == right);
+        }
+    }
+}
