947d7ca1a8cd6bcfc10beaad5732d21c2b2724c5
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-25 22:40:53 +0100

    Solve 2015 Day 3 Silver

--- Source/App/Advents/2015/Day3/Question.cs
+++ Source/App/Advents/2015/Day3/Question.cs
@@ -1,4 +1,7 @@
-﻿using App.Common.Question;
+﻿using App.Common.Exceptions;
+using App.Common.Input;
+using App.Common.Math;
+using App.Common.Question;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +10,65 @@ using System.Threading.Tasks;
 
 namespace App.Advents._2015.Day3
 {
+    /// <summary>
+    /// https://adventofcode.com/2015/day/3
+    /// </summary>
     public class Question : QuestionBase<int>
     {
+        private Dictionary<Vector2Int, int> _visited;
+
+        private Vector2Int _santaPos;
+
+        internal override int SolveSilver(string path)
+        {
+            var input = InputHelper.GetText(path);
+
+            _santaPos = new();
+            _visited = new();
+            foreach (var move in input)
+            {
+                HandleMove(move);
+
+                DeliverPresent();
+            }
+
+            return _visited.Count;
+        }
+
         internal override int SolveGold(string path)
         {
             throw new NotImplementedException();
         }
 
-        internal override int SolveSilver(string path)
+        private void DeliverPresent()
         {
-            throw new NotImplementedException();
+            if (!_visited.ContainsKey(_santaPos))
+            {
+                _visited.Add(_santaPos, 0);
+            }
+
+            _visited[_santaPos]++;
+        }
+
+        private void HandleMove(char move)
+        {
+            switch (move)
+            {
+                case '^':
+                    _santaPos.Y++;
+                    break;
+                case 'v':
+                    _santaPos.Y--;
+                    break;
+                case '<':
+                    _santaPos.X--;
+                    break;
+                case '>':
+                    _santaPos.X++;
+                    break;
+                default:
+                    throw new InvalidCharacterException(move);
+            }
         }
     }
 }
