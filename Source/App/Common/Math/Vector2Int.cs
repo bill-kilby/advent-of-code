a2ca56c0a21d89ad7a15f5904f178c6325c37876
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-25 23:28:04 +0100

    Polish Vector2Int to include common usages
    Inspired by: https://github.com/Unity-Technologies/UnityCsReference/blob/master/Runtime/Export/Math/Vector2Int.cs

--- Source/App/Common/Math/Vector2Int.cs
+++ Source/App/Common/Math/Vector2Int.cs
@@ -1,48 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace App.Common.Math
 {
-    internal class Vector2Int
+    public readonly struct Vector2Int(int x, int y) : IEquatable<Vector2Int>
     {
-        public int X;
-        public int Y;
+        public int X { get; } = x;
+        public int Y { get; } = y;
 
-        public override bool Equals(object? obj)
-        {
-            if (obj is Vector2Int other)
-            {
-                return X == other.X && Y == other.Y;
-            }
-            return false;
-        }
+        public static Vector2Int Up = new(0, 1);
+        public static Vector2Int Down = new(0, -1);
+        public static Vector2Int Left = new(-1, 0);
+        public static Vector2Int Right = new(1, 0);
 
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 17;
-                hash = hash * 31 + X;
-                hash = hash * 31 + Y;
-                return hash;
-            }
-        }
+        public override string ToString() => $"{X},{Y}";
 
-        public static bool operator ==(Vector2Int left, Vector2Int right)
-        {
-            if (ReferenceEquals(left, right))
-                return true;
-            if (left is null || right is null)
-                return false;
-            return left.Equals(right);
-        }
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+        public override bool Equals(object? obj) => obj is Vector2Int other && Equals(other);
+        public bool Equals(Vector2Int other) => X == other.X && Y == other.Y;
 
-        public static bool operator !=(Vector2Int left, Vector2Int right)
-        {
-            return !(left == right);
-        }
+        public static Vector2Int operator /(Vector2Int a, int b) => new Vector2Int(a.X / b, a.Y / b);
+        public static Vector2Int operator -(Vector2Int v) => new Vector2Int(-v.X, -v.Y);
+        public static Vector2Int operator +(Vector2Int a, Vector2Int b) => new Vector2Int(a.X + b.X, a.Y + b.Y);
+        public static Vector2Int operator -(Vector2Int a, Vector2Int b) => new Vector2Int(a.X - b.X, a.Y - b.Y);
+        public static Vector2Int operator *(Vector2Int a, Vector2Int b) => new Vector2Int(a.X * b.X, a.Y * b.Y);
+        public static Vector2Int operator *(int a, Vector2Int b) => new Vector2Int(a * b.X, a * b.Y);
+        public static Vector2Int operator *(Vector2Int a, int b) => new Vector2Int(a.X * b, a.Y * b);
+
+        public static bool operator ==(Vector2Int left, Vector2Int right) => left.Equals(right);
+        public static bool operator !=(Vector2Int left, Vector2Int right) => !left.Equals(right);
     }
 }
