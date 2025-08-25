02c82cac7be161d1f312cd60fa6c424d6bbc7393
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-26 00:22:51 +0100

    Solve 2015 Day 5 Silver

--- Source/App/Advents/2015/Day5/Question.cs
+++ Source/App/Advents/2015/Day5/Question.cs
@@ -1,4 +1,6 @@
-﻿using App.Common.Question;
+﻿using App.Common.Abstractions;
+using App.Common.Input;
+using App.Common.Question;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +9,27 @@ using System.Threading.Tasks;
 
 namespace App.Advents._2015.Day5
 {
+    /// <summary>
+    /// https://adventofcode.com/2015/day/5
+    /// </summary>
     public class Question : QuestionBase<int>
     {
-        internal override int SolveGold(string path)
-        {
-            throw new NotImplementedException();
-        }
+        private IValidator<string> _validator = new NicenessValidator();
 
         internal override int SolveSilver(string path)
+        {
+            var input = InputHelper.GetLines(path);
+
+            var count = 0;
+            foreach (var line in input)
+            {
+                if (_validator.Validate(line)) count++;
+            }
+
+            return count;
+        }
+
+        internal override int SolveGold(string path)
         {
             throw new NotImplementedException();
         }
