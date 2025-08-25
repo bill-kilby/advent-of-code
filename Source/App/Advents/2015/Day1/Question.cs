f6e7fd41d1bcabd58a67dad36b061c7380627dbc
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-25 19:55:52 +0100

    Add template question

---
+++ Source/App/Advents/2015/Day1/Question.cs
@@ -0,0 +1,22 @@
+﻿using App.Common.Question;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Advents._2015.Day1
+{
+    public class Question : QuestionBase<string>
+    {
+        internal override string SolveSilver(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override string SolverGold(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
