65e6c4e24d54fc9d5f1f415fa10cd2f101f1764b
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-25 21:44:27 +0100

    Add question base

---
+++ Source/App/Advents/2015/Day2/Question.cs
@@ -0,0 +1,27 @@
+﻿using App.Common.Question;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Advents._2015.Day2
+{
+    /// <summary>
+    /// https://adventofcode.com/2015/day/2
+    /// </summary>
+    public class Question : QuestionBase<int>
+    {
+        internal override int SolveSilver(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override int SolveGold(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Source/Test/Advents/2015/Day2/QuestionTests.cs
@@ -0,0 +1,22 @@
+﻿using App.Advents._2015.Day2;
+using Test.Common;
+
+namespace Test.Advents._2015.Day2
+{
+    internal class QuestionTests : IQuestionTests<int>
+    {
+        [TestCase("2015/Day2/input.txt", 1588178, 3783758)]
+        public void SolvingQuestion_ReturnsCorrectResult(string path, int silverAnswer, int goldAnswer)
+        {
+            // Assemble
+            var question = new Question();
+
+            // Act
+            var answer = question.Solve(path);
+
+            // Assert
+            Assert.That(answer.Silver, Is.EqualTo(silverAnswer));
+            Assert.That(answer.Gold, Is.EqualTo(goldAnswer));
+        }
+    }
+}
