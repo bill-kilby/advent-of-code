1d36fbecd2cfe8e76855fcf1e2a9b7bce0a68e54
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-26 13:42:54 +0100

    GoldValidatorTests

---
+++ Source/App/Advents/2015/Day5/GoldValidator.cs
@@ -0,0 +1,17 @@
+﻿using App.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Advents._2015.Day5
+{
+    public class GoldValidator : IValidator<string>
+    {
+        public bool Validate(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
