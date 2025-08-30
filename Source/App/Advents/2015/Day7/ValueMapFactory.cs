1ddcd861a7151bf1b89f5164b2a06e7c5d59aa8b
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-30 15:38:50 +0100

    Implement ValueMapFactory

---
+++ Source/App/Advents/2015/Day7/ValueMapFactory.cs
@@ -0,0 +1,39 @@
+﻿using App.Advents._2015.Day7.Instructions;
+using App.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Advents._2015.Day7
+{
+    public class ValueMapFactory : IFactory<Dictionary<string, int>, Instruction[]>
+    {
+        private Dictionary<string, int> _map = new();
+
+        public Dictionary<string, int> Create(Instruction[] input)
+        {
+            _map = new();
+
+            foreach (var instruction in input)
+            {
+                MapInstruction(instruction);
+            }
+
+            return _map;
+        }
+
+        private void MapInstruction(Instruction instruction)
+        {
+            foreach (var input in instruction.Inputs)
+            {
+                if (!_map.Keys.Contains(input))
+                    _map.Add(input, int.MinValue);
+            }
+
+            if (!_map.Keys.Contains(instruction.Output))
+                _map.Add(instruction.Output, int.MinValue);
+        }
+    }
+}
