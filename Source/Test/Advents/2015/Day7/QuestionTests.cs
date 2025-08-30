2f910ba320cd500c0fa7b8f80d43cac4e03cb968
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-30 13:32:24 +0100

    Add base question

---
+++ Source/Test/Advents/2015/Day7/QuestionTests.cs
@@ -0,0 +1,27 @@
+﻿using App.Advents._2015.Day7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Common;
+
+namespace Test.Advents._2015.Day7
+{
+    internal class QuestionTests : IQuestionTests<int>
+    {
+        [TestCase("2015/Day7/input.txt", 543903, 14687245)]
+        public void SolvingQuestion_ReturnsCorrectResult(string path, int silverAnswer, int goldAnswer)
+        {
+            // Assemble
+            var question = new Question();
+
+            // Act
+            var answer = question.Solve(path);
+
+            // Assert
+            Assert.That(answer.Silver, Is.EqualTo(silverAnswer));
+            Assert.That(answer.Gold, Is.EqualTo(goldAnswer));
+        }
+    }
+}
