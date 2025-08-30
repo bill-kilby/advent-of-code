3087457bd7dfafa3f3ae56b36b37b6cc270b2580
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-30 14:02:32 +0100

    Implement InstructionFactory tests

---
+++ Source/App/Advents/2015/Day7/Instructions/InstructionFactory.cs
@@ -0,0 +1,17 @@
+﻿using App.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Advents._2015.Day7.Instructions
+{
+    public class InstructionFactory : IFactory<Instruction>
+    {
+        public Instruction Create(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
