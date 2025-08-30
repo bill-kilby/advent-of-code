3a5dce81130b7a4effea65a53d10950923b493b6
bill-kilby <billo.kilby@yahoo.co.uk>
2025-08-30 01:17:26 +0100

    2015 Day 6 Silver Solution

--- Source/App/Advents/2015/Day6/Question.cs
+++ Source/App/Advents/2015/Day6/Question.cs
@@ -1,9 +1,13 @@
-﻿using App.Common.Question;
+﻿using App.Advents._2015.Day6.Instructions;
+using App.Common.Abstractions;
+using App.Common.Input;
+using App.Common.Question;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Test.Advents._2015.Day6;
 
 namespace App.Advents._2015.Day6
 {
@@ -12,14 +16,49 @@ namespace App.Advents._2015.Day6
     /// </summary>
     public class Question : QuestionBase<int>
     {
+        private ILightMap _map = new LightMap();
+        private IFactory<Instruction> _instructionFactory = new InstructionFactory();
+
+        internal override int SolveSilver(string path)
+        {
+            _map = new LightMap();
+
+            var input = InputHelper.GetLines(path);
+
+            foreach (var line in input)
+            {
+                var instruction = _instructionFactory.Create(line);
+                DoInstruction(instruction);
+            }
+
+            return _map.GetTotalLitLights();
+        }
+
         internal override int SolveGold(string path)
         {
             throw new NotImplementedException();
         }
 
-        internal override int SolveSilver(string path)
+        private void DoInstruction(Instruction instruction)
         {
-            throw new NotImplementedException();
+            foreach (var light in instruction.AffectedLights)
+            {
+                switch (instruction.InstructionType)
+                {
+                    case InstructionType.TurnOn:
+                        _map.TurnOn(light);
+                        break;
+                    case InstructionType.TurnOff:
+                        _map.TurnOff(light);
+                        break;
+                    case InstructionType.Toggle:
+                        _map.Toggle(light);
+                        break;
+                    default:
+                        throw new NotImplementedException(
+                            $"Unknown instruction type is unimplemented: {instruction.InstructionType}.");
+                }
+            }
         }
     }
 }
