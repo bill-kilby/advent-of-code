293b581a011031858f448b8e7ac9bbf7600fd24b
bill-kilby <billo.kilby@yahoo.co.uk>
2025-09-01 20:40:38 +0100

    Add question base

---
+++ Source/App/Advents/2015/Day8/Question.cs
@@ -0,0 +1,22 @@
+﻿using App.Common.Question;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Advents._2015.Day8
+{
+    public class Question : QuestionBase<int>
+    {
+        protected override int SolveGold(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int SolveSilver(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
